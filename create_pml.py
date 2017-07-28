2999259a78cc8b5bb00a819e74318ffc729757c4
ilsenatorov <il.senatorov@gmail.com>
2017-07-28 17:10:09 +0200

    some minor tweaks
    added some comments, renamed some variables for easier use

--- create_pml.py
+++ create_pml.py
@@ -1,8 +1,7 @@
 """
 this is supposed to create a .pml from a string of data
-need to organise the regex into a callable function
-add the pdb inside the pml
-tests from perl script:
+need to organise the regex into a callable function, need to fimish the .pml compiler
+test from perl script:
 10gs D2-78[A]+187-208[A] D79-186[A] F209-209[A]
 1a35A D236-319[A] D320-430[A] D431-580[A] D591-635[A]+713-764[A] F215-235[A] F581-590[A] F765-765[A]
 1a36 D2-215[A]+320-431[A] D232-319[A] D432-583[A] D584-765[A]
@@ -14,16 +13,19 @@ the_string = '''
 1a35A D236-319[A] D320-430[A] D431-580[A] D591-635[A]+713-764[A] F215-235[A] F581-590[A] F765-765[A]
 '''
 
-pdb_idRegex = re.compile(r'\w{4,5}')
+pdb_id_wholeRegex = re.compile(r'\d\w{3}')
+pdb_id_chainRegex = re.compile(r'\d\w{4}')
 whole_domainRegex = re.compile(r'D\d+-\d+\S*')
 fragmentRegex = re.compile(r'F\d{1,10}-\d{1,10}')
 coordinatesRegex = re.compile(r'\d+-\d+')
+
+
 list_of_colours = ["Blue", "Red", "Green", "Yellow", "Pink", "Grey"]
 
-pdb_id = pdb_idRegex.findall(the_string)
+pdb_id_whole = pdb_id_wholeRegex.search(the_string).group()
+pdb_id_chain = pdb_id_chainRegex.search(the_string).group()
 domains = whole_domainRegex.findall(the_string)
 fragments = fragmentRegex.findall(the_string)
-coordinates = coordinatesRegex.findall(the_string)
 
 def fetch_domains(list_of_strings): #returns a dictionary of domains and coordinates
     dict_domains = {}
@@ -40,15 +42,18 @@ def fetch_fragments(list_of_fragments): #returns a list of coordinates that are 
         fragment_list.append(coordinatesRegex.search(fragment).group())
     return fragment_list
 
+def pdb_parser():
+    pdb_file = open('C:\\Users\\Ilya\\PycharmProjects\\pymol\\' + pdb_id_chain, 'r')
+
 def create_pymol(): #will compile all the data into a .pml file
-    pymol_script = open('C:\\Users\\Ilya\\PycharmProjects\\pymol\\pymolscript.pml', 'w')
-    pymol_script.write("fetch " + pdb_id[0] + ", async=0\n" + "select all\ncolour white\ndeselect\n")
+    pymol_script = open('C:\\Users\\Ilya\\PycharmProjects\\pymol\\' + pdb_id_chain +'_chopping' '.pml', 'w')
+    pymol_script.write("fetch " + pdb_id_chain + ", async=0\n" + "select all\ncolour White\ndeselect\n")
     count = 1
     number_of_doms = len(fetch_domains(domains))
-    for domain in range(number_of_doms):
+    for domain in range(number_of_doms): #puts all domains in .pml
         pymol_script.write("select domain" + str(count) + ", ")
-        for coordin in fetch_domains(domains)["domain " + str(count)]:
-            if coordin == fetch_domains(domains)["domain " + str(count)][-1]:
+        for coordin in fetch_domains(domains)["domain " + str(count)]:#puts all pieces of a single domain in .pml
+            if coordin == fetch_domains(domains)["domain " + str(count)][-1]: #doesnt add a + if it is the last piece
                 pymol_script.write("resi " + coordin)
                 break
             pymol_script.write("resi " + coordin + " + ")
@@ -57,17 +62,19 @@ def create_pymol(): #will compile all the data into a .pml file
     for domain in range(number_of_doms):
         pymol_script.write("colour " + list_of_colours[domain] + (", domain") + str(domain + 1) + "\n")
     pymol_script.write("select fragments, ")
-    for fragment in fetch_fragments(fragments):
-        if fragment == fetch_fragments(fragments)[-1]:
+    for fragment in fetch_fragments(fragments): #puts all fragments in .pml
+        if fragment == fetch_fragments(fragments)[-1]: #doesnt add a + if it is the last fragment
             pymol_script.write("resi " + fragment)
             break
         pymol_script.write("resi " + str(fragment) + " + ")
-    pymol_script.write("\ncolour Purple, fragments\n")
-    pymol_script.write("hide all\ndeselect\nshow cartoon\ndelete sele\n")
+    pymol_script.write("\ncolour White, fragments\n")
+    pymol_script.write("hide all\ndeselect\ndelete sele\nshow cartoon\n")
 
 
 
-print(pdb_id[0])
+
+print(pdb_id_whole)
+print(pdb_id_chain)
 print(fetch_domains(domains))
 print(fragments)
 
